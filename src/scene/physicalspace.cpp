b949669da46df20ab79c43db0fbcb8febf87c03d
Zhennan Tu <zhennan.tu@gmail.com>
2017-05-26 16:08:31 +0800

    崩溃修复

--- src/scene/physicalspace.cpp
+++ src/scene/physicalspace.cpp
@@ -43,13 +43,22 @@ void PhysicalSpace::delModel(shared_ptr<Entity> entity)
     mp_.erase(it);
 }
 
-bool PhysicalSpace::collision(shared_ptr<Model> model, int32_t x, int32_t y)
+//collision()检测：当该Model位移(movement_x, movement_y)时，是否与场景内其它物体碰撞
+//是返回true，否返回false
+//该函数修修补补改了好几遍，逻辑写得有些混乱，日后需要重写
+bool PhysicalSpace::collision(shared_ptr<Model> model, int32_t movement_x, int32_t movement_y)
 {
-    int32_t nx = x / BLOCK_SIZE;
-    int32_t ny = y / BLOCK_SIZE;
+    int32_t mx = movement_x / BLOCK_SIZE;
+    int32_t my = movement_y / BLOCK_SIZE;
     for (auto& p : model->pos)
     {
-        Block bk = grid_[ny+p.y][nx+p.x];
+        //如果没有下面这个if语句，小人走到上边界和下边界程序会崩溃，
+        //原因是下面的 Block& bk = grid_[my+p.y][mx+p.x] 越界了
+        if (my+p.y >= height_|| mx+p.x >= width_)
+        {
+            return true;
+        }
+        Block& bk = grid_[my+p.y][mx+p.x];
         if (bk.owner != mp_[model] && bk.solid == true)
         {
             return true;
@@ -58,6 +67,8 @@ bool PhysicalSpace::collision(shared_ptr<Model> model, int32_t x, int32_t y)
     return false;
 }
 
+
+//下面这三个函数，都是为了修补collision()崩溃而写的，有些帮上了忙，有些则不
 PhysicalSpace::Block::Block(const PhysicalSpace::Block& block)
 {
     this->solid = block.solid;
@@ -66,15 +77,19 @@ PhysicalSpace::Block::Block(const PhysicalSpace::Block& block)
 }
 PhysicalSpace::Block& PhysicalSpace::Block::operator=(const PhysicalSpace::Block& block)
 {
-    this->solid = block.solid; this->harm = block.harm; this->owner.reset(block.owner.get()); return *this;
+    this->solid = block.solid;
+    this->harm = block.harm;
+    std::cout<<"Block::operator=: "<<owner.get()<<std::endl;
+    this->owner.reset(block.owner.get());
+    return *this;
 }
 
 PhysicalSpace::Block::~Block()
 {
     if (owner.get() != nullptr)
     {
-        this->owner.reset();
         std::cout<<"owner:"<<owner<<std::endl;
+        this->owner.reset();
     }
-    std::cout<<"owner: empty"<<std::endl;
+    //std::cout<<"owner: empty"<<std::endl;
 }
